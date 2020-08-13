ce2c8270ea1866a393c1d33bbcf8a852440c9107
Eka1755201182 <yuliekasafitri7@gmail.com>
2020-08-13 16:18:55 +0700

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    YULI EKA SAFITRI
+                    YULI EKA SAFITRi
                 </div>
 
                 <div class="links">
