20b077253d20a6c9b3539bc21bd18d9ad6a6814a
Eka1755201182 <yuliekasafitri7@gmail.com>
2020-08-13 16:38:37 +0700

    Pertemuan 10: Install Laravel dan Upload Github

--- resources/views/layouts/alert.blade.php
+++
@@ -1,8 +0,0 @@
-@if($message = Session::get('success'))
-        <!-- Alert -->
-        <p>
-            <div class="alert alert-success">
-                <p>{{ $message }}</p>
-            </div>
-        </p>
-   @endif
